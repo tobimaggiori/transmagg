3ed7e0cf0b7ebb6d1bdf6c4d30b86b09f86e4b74
tobimaggiori <tobiasmaggiori@outlook.com>
2025-12-22 23:16:27 -0300

    a

--- app/models/usuario_model.py
+++ app/models/usuario_model.py
@@ -1,26 +1,29 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime
-# El .. le dice a Python: "Anta atrás una carpeta y buscá database.py"
 from ..database import Base
+from datetime import datetime
 
 class Usuario(Base):
     __tablename__ = "usuarios"
-
+    # Perfil de usuario
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    nombre = Column(String)
-    celular = Column(String)
-    rol = Column(String, default="usuario")
-    activo = Column(Boolean, default=True)
-
-    # Login sin contrasena
-    codigo = Column(String, nullable=True)
+    username = Column(String(50), nullable=False)
+    rol = Column(String(30), nullable=False)
+    email = Column(String(120), unique=True, index=True, nullable=False)
+    activo = Column(Boolean, nullable=False, default=True)
+    # Login sin contraseña
+    codigo = Column(String(80), nullable=True)
     tiempo = Column(DateTime, nullable=True)
-
-    # Relaciones
+    # Relaciones (FKs)
     fletero_id = Column(Integer, ForeignKey("fleteros.id"), nullable=True)
- #   empresa_id = Column(Integer, ForeignKey("empresas.id"), nullable=True)
- #   chofer_id = Column(Integer, ForeignKey("choferes.id"), nullable=True)
-
+    empresa_id = Column(Integer, ForeignKey("empresas.id"), nullable=True)
+    chofer_id = Column(Integer, ForeignKey("choferes.id"), nullable=True)
+    # Auditoría
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(
+        DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
     def vincular_fletero():
         None
     def vincular_empresa():
