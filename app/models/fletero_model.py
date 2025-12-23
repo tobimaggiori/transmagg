5c1d32e733f5046918e92f3f15ff72fa3f369799
tobimaggiori <tobiasmaggiori@outlook.com>
2025-12-22 23:26:55 -0300

    modificacion fletero_model.py

--- app/models/fletero_model.py
+++ app/models/fletero_model.py
@@ -1,7 +1,22 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, Column, DateTime, Integer, String
+from datetime import datetime
 from ..database import Base
 
 class Fletero(Base):
     __tablename__ = "fleteros"
+
     id = Column(Integer, primary_key=True)
     cuit = Column(String, unique=True)
+    razon_social = Column(String(150), nullable=False)
+    cuit = Column(String(11), unique=True, nullable=False)
+    provincia_id = Column(Integer, nullable=False)
+    localidad_id = Column(Integer, nullable=False)
+    direccion = Column(String(200))
+    cpostal = Column(String(10))
+    activo = Column(Boolean, nullable=False, default=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(
+        DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+    )
